237482d04741995120bc4660b7f5c5eed8b2f534
Saddam <sddmhossain@786gmail.com>
2024-01-24 08:52:32 +0530

    ArrayList code updated

---
+++ src/test/java/collectionframework/ArrayListDemo.java
@@ -0,0 +1,25 @@
+package collectionframework;
+
+import java.util.ArrayList;
+public class ArrayListDemo {
+   static String[] students = new String[4];
+   static int[] nums = new int[4];
+    public static void main(String[] args) {
+//        ArrayList employees;
+
+        ArrayList companies = new ArrayList();
+
+//        to get the size of the arraylist we have to use size()
+        System.out.println(companies.size());
+
+        int count = 0;
+        while (count < 10) {
+            companies.add("Company" + count);
+            count++;
+        }
+        System.out.println(companies);
+        System.out.println(companies.size());
+        companies.add(3, "TCS");
+        System.out.println(companies);
+    }
+}
