f0ad2958b9477c4223a59edd91c1ef8760ae2c45
Saddam <sddmhossain@786gmail.com>
2024-01-27 20:10:15 +0530

    WebTableHandling1 code updated

--- src/test/java/SeleniumPractice/webtable/WebTableHandling1.java
+++ src/test/java/SeleniumPractice/webtable/WebTableHandling1.java
@@ -2,10 +2,14 @@ package SeleniumPractice.webtable;
 
 import SeleniumPractice.SeleniumBasicActions;
 import helperlibrary.SeleniumHelper;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class WebTableHandling1 extends SeleniumBasicActions {
     WebDriver driver;
@@ -23,5 +27,34 @@ public class WebTableHandling1 extends SeleniumBasicActions {
     public void handleWebTable(){
         seleniumHelper = new SeleniumHelper(driver);
         seleniumHelper.clickOnElement("//a[.='Books List']");
+//        fething data from the webtable
+        int rowNum = 0;
+        int colNum =0;
+        String cellXpath = "//table/tbody/tr["+rowNum+"]/td["+colNum+"]";
+        int numOfRows = driver.findElements(By.xpath("//table/tbody/tr")).size();
+        int numOfCols = driver.findElements(By.xpath("//table/thead/tr/th")).size();
+
+        for (rowNum = 1; rowNum <= numOfRows; rowNum++) {
+            for (colNum = 1; colNum <= numOfCols; colNum++) {
+                System.out.print(driver.findElement(
+                        By.xpath("//table/tbody/tr["+rowNum+"]/td["+colNum+"]")).getText() + " || ");
+            }
+            System.out.println("\n");
+        }
+
+        List<WebElement> authorsList =  driver.findElements(By.xpath("//table/tbody/tr/td[2]"));
+
+        int targetRowNum = 0;
+        for (int i = 0; i<authorsList.size(); i++){
+            if (authorsList.get(i).getText().equals("William Johnson"))
+                targetRowNum = i+1;
+        }
+
+        System.out.println("---------------------------------------------------------------------");
+
+        List<WebElement> celData = driver.findElements(By.xpath("//table/tbody/tr["+ targetRowNum +"]/td"));
+
+        for (WebElement cell : celData)
+            System.out.print(cell.getText() + " ->> ");
     }
 }
--- src/test/java/helperlibrary/SeleniumHelper.java
+++ src/test/java/helperlibrary/SeleniumHelper.java
@@ -20,4 +20,6 @@ public class SeleniumHelper {
         getElement(xpath).click();
     }
 
+//    public
+
 }
