bb0790b779d462449e8d390a69b630f375e7afc0
Saddam <sddmhossain@786gmail.com>
2024-02-13 09:10:36 +0530

    MethodOverLoading Script added

---
+++ src/test/java/test/MyClass.java
@@ -0,0 +1,11 @@
+package test;
+
+public class MyClass {
+    public void methodM1(String str) {
+        System.out.println("Inside String M1");
+    }
+
+    public void methodM1(int num) {
+        System.out.println("Inside Numeric M1");
+    }
+}
---
+++ src/test/java/test/MyClassTest.java
@@ -0,0 +1,8 @@
+package test;
+
+public class MyClassTest {
+    public static void main(String[] args) {
+        MyClass myClass = new MyClass();
+        myClass.methodM1(null);
+    }
+}
